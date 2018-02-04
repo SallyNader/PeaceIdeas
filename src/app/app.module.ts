f66bda2b0f13f3ef2014833b93349fb88331e657
sallyNader <s.ahmad@vardot.com>
2018-02-04 17:08:21 +0200

    initiate project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,27 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { ProfileComponent } from './profile/profile.component';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { CommentComponent } from './comment/comment.component';
+import { IdeaComponent } from './idea/idea.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    ProfileComponent,
+    RegisterComponent,
+    LoginComponent,
+    HomeComponent,
+    CommentComponent,
+    IdeaComponent
   ],
   imports: [
     BrowserModule
